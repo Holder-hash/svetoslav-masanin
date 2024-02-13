b9103a9962ad7375a5795125776934e38b58fb52
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-02-13 12:37:21 +0300

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import HomePage from "./screens/HomePage/HomePage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/svetoslav-masanin" element={<Layout />}></Route>
+      <Route path="/svetoslav-masanin" element={<Layout />}>
+        <Route index element={<HomePage />} />
+      </Route>
     </Routes>
   );
 }
