5dd9de24a52ca5ea47505a910f12ee2c8a584b30
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-02-13 12:24:25 +0300

    header style

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,9 +1,34 @@
 import styles from "./Header.module.scss";
+import logoImg from "../../../assets/images/logo.jpg";
+import { GrDocumentText } from "react-icons/gr";
+import { FaHome } from "react-icons/fa";
+import { LuLightbulbOff } from "react-icons/lu";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <div></div>
+      <div className={styles.headerInner}>
+        <div className={styles.logo}>
+          <img src={logoImg} alt='ГБПОУ Колледж "Подмосковье"' />
+        </div>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <FaHome className={styles.navIcon} />
+              Главная
+            </li>
+            <li>
+              <GrDocumentText className={styles.navIcon} />
+              Лекционные материалы
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles.themeIcon}>
+          <LuLightbulbOff />
+        </div>
+      </div>
     </header>
   );
 }
