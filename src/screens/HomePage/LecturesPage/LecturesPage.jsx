63dc12a0193b8c9e9d99fde2e320af80eed5715f
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-02-18 17:12:22 +0300

    lectures page marking

--- src/screens/HomePage/LecturesPage/LecturesPage.jsx
+++ src/screens/HomePage/LecturesPage/LecturesPage.jsx
@@ -3,7 +3,18 @@ import styles from "./LecturesPage.module.scss";
 function LecturesPage() {
   return (
     <>
-      <div></div>
+      <div className={styles.wrapper}>
+        <aside className={styles.aside}>
+          <h3>Навигация</h3>
+
+          <details>
+            <summary>Легенда</summary>
+            <p>Раскрывающийся текст</p>
+          </details>
+        </aside>
+
+        <div className={styles.content}></div>
+      </div>
     </>
   );
 }
