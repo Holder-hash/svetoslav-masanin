a36f7f32288f0912dadc81bcb1dbf3517429a8f4
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-02-20 19:48:47 +0300

    lectures content

--- src/screens/LecturesPage/LecturesPage.jsx
+++ src/screens/LecturesPage/LecturesPage.jsx
@@ -1,9 +1,34 @@
 import styles from "./LecturesPage.module.scss";
+import lectures from "../../data/lectures.json";
 
 function LecturesPage() {
   return (
     <>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <h2>Выберите материал для изучения</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          autem sed doloremque, repellendus soluta aliquam minima saepe
+          accusantium. Harum at enim natus. Ratione labore sunt reprehenderit
+          sed tempora laboriosam quisquam fugiat id et saepe dolorem
+          necessitatibus delectus unde quaerat in commodi alias nulla
+          aspernatur, laudantium dolor quia magnam! Sunt, praesentium.
+        </p>
+        <div className={styles.cardsWrapper}>
+          {lectures.map((lecture) => (
+            <div key={lecture.id} className={styles.card}>
+              <img src={lecture.img} alt="" />
+              <div className={styles.cardTitleBlock}>
+                <h2>{lecture.title}</h2>
+                <p>{lecture.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <p className={styles.copy}>
+          Автором предоставленных материалов является Светосла Масанин
+        </p>
+      </div>
     </>
   );
 }
